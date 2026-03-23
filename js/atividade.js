5aa48533deb269875565e967f8684f00a4c64a4b
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-23 09:45:46 -0300

    feat: Exercício de fixação pedido em sala de aula

---
+++ js/atividade.js
@@ -0,0 +1,12 @@
+var nome = "Leonardo";
+var sobrenome = "Santos";
+var anoAtual = 2026;
+var idade = 18;
+
+var nomeSobrenome = nome + " " + sobrenome;
+console.log(nomeSobrenome);
+
+var anoNascimento = anoAtual - idade;
+
+console.log(anoNascimento);
+
--- js/script.js
+++ js/script.js
@@ -1,77 +1,77 @@
-// // tipos primitivos
-// // boolean
-// var boolean = false;
-// console.log("Texto " + boolean + " texto");
-// // template string
-// console.log(`A variável ${boolean} tem o tipo ${typeof(boolean)}`);
+// // // tipos primitivos
+// // // boolean
+// // var boolean = false;
+// // console.log("Texto " + boolean + " texto");
+// // // template string
+// // console.log(`A variável ${boolean} tem o tipo ${typeof(boolean)}`);
 
-// var numero = 1;
-// // ctrl + d = seleciona a próxima palavra igual
-// console.log(`A variável ${numero} tem o tipo ${typeof(numero)}`);
+// // var numero = 1;
+// // // ctrl + d = seleciona a próxima palavra igual
+// // console.log(`A variável ${numero} tem o tipo ${typeof(numero)}`);
 
-// var; //variável global ou local - tipo valor inicial pode ser nulo
-// let; //variável local de bloco - valor inicial pode ser nulo
-// const; //variável local de bloco - valor inicial é obrigatório
+// // var; //variável global ou local - tipo valor inicial pode ser nulo
+// // let; //variável local de bloco - valor inicial pode ser nulo
+// // const; //variável local de bloco - valor inicial é obrigatório
 
-// var prof = "lucas";
-// var prof = "lucas Sousa";
+// // var prof = "lucas";
+// // var prof = "lucas Sousa";
 
-// let sobrenome = "Sousa";
+// // let sobrenome = "Sousa";
 
-// sobrenome = "Borges de Sousa";
+// // sobrenome = "Borges de Sousa";
 
-// const profissao = "professor";
+// // const profissao = "professor";
 
-// profissao = "programador";
+// // profissao = "programador";
 
-// const profissao = "testador";
+// // const profissao = "testador";
 
-// usando variável global
-var nome = "Lucas";
+// // usando variável global
+// var nome = "Lucas";
 
-// usando variável local
-function nomeDaFuncao() {
-    var sobrenome = "Sousa";
-    console.log(sobrenome);
+// // usando variável local
+// function nomeDaFuncao() {
+//     var sobrenome = "Sousa";
+//     console.log(sobrenome);
     
-}
+// }
 
-console.log(nome);
-nomeDaFuncao();
+// console.log(nome);
+// nomeDaFuncao();
 
-// operadores de comparação
-var comparacao = "0" == 0;
-console.log(comparacao);
+// // operadores de comparação
+// var comparacao = "0" == 0;
+// console.log(comparacao);
 
-var comparacaoIdentica = "0" === 0;
-console.log(comparacaoIdentica);
+// var comparacaoIdentica = "0" === 0;
+// console.log(comparacaoIdentica);
 
-// operadores aritméticos
-// +, -, *, /, %, **
+// // operadores aritméticos
+// // +, -, *, /, %, **
 
-var mult = 5 * 9
-console.log(mult);
+// var mult = 5 * 9
+// console.log(mult);
 
-var divisao = 15/3;
-console.log(divisao);
+// var divisao = 15/3;
+// console.log(divisao);
 
 
-// operadores relacionais 
-// >, <, >=, <=, !=
-// maior ou igual
-var maiorOuIgual = 5 >= 16;
-console.log(maiorOuIgual, typeof(maiorOuIgual));
+// // operadores relacionais 
+// // >, <, >=, <=, !=
+// // maior ou igual
+// var maiorOuIgual = 5 >= 16;
+// console.log(maiorOuIgual, typeof(maiorOuIgual));
 
-var diferente = 5 != 8;
-console.log(diferente, typeof(diferente));
+// var diferente = 5 != 8;
+// console.log(diferente, typeof(diferente));
 
-// operadores lógicos
-// && -- E, || -- ou, ! -- não
-var e = true && true && false;
-console.log(e);
+// // operadores lógicos
+// // && -- E, || -- ou, ! -- não
+// var e = true && true && false;
+// console.log(e);
 
-var ou = true || false || false;
-console.log(ou);
+// var ou = true || false || false;
+// console.log(ou);
 
-var nao = !true;
-console.log(nao);
+// var nao = !true;
+// console.log(nao);
